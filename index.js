5a4ff1d3fbab7c79ba670bbf0cf4989550f0de7f
UlugbekgoldTASHKENT <ulugbekmirzaliyev77@gmail.com>
2022-12-26 20:07:51 +0500

    sdasada

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let mehmonlar = 'aziz'
+// // function kasib(yili) {
+// // return (yili % 100 === 0) ? (yili % 400 === 0) : (yili % 4 === 0);
 
-let mehmonlar1 =['polat', 'jasur','mamur']
+// }
+// // console.log(kasib(2016));
+// // console.log(kasib(2000));
+// // console.log(kasib(1700));
+// // console.log(kasib(1800));
+// // console.log(kasib(100));
+let INPUTTXT = (-10. 10)
+let result = INPUTTXT.repLace('10', '-10') 
+console.log(result)
